c4afcb92c5c38dc5c7be057fbedf4b42b6adfe65
Basam Hassan <basammoses@gmail.com>
2023-04-11 09:35:34 -0400

    comma fix

--- api/refurb/settings.py
+++ api/refurb/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    ,
+    
     'django_extensions',
     'import_export',
     'corsheaders',
