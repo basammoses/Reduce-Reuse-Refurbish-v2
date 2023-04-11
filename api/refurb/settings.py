298de6625823aad54737f577adc042763e611379
Basam Hassan <basammoses@gmail.com>
2023-04-11 09:48:32 -0400

    static directory fix

--- api/refurb/settings.py
+++ api/refurb/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -252,3 +253,5 @@ CHANNEL_LAYERS = {
     }
 }
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
