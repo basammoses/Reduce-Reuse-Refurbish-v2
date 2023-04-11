e81b02488e50aaa4dbca93286feb7e2bd12f22db
Basam Hassan <basammoses@gmail.com>
2023-04-11 09:35:22 -0400

    removed static

--- api/refurb/settings.py
+++ api/refurb/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    ,
     'django_extensions',
     'import_export',
     'corsheaders',
@@ -151,7 +150,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -254,5 +252,3 @@ CHANNEL_LAYERS = {
     }
 }
 
-
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
